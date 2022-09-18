75368ccf2ce2bfe5b6bbfee890e204943e03740a
Alexander Silin <sasha.silin97@gmail.com>
2022-09-19 00:59:00 +0300

    refactor: create generic onboarding view controller
    This commit adds a generic onboarding view controller that can be reused across multiple onboarding screens.

--- Bankey/Bankey/OnboardingViewController.swift
+++ Bankey/Bankey/OnboardingViewController.swift
@@ -3,6 +3,9 @@ import UIKit
 
 class OnboardingViewController: UIViewController {
     
+    let heroImageName: String
+    let titleText: String
+    
     let stackView = UIStackView()
     let imageView = UIImageView()
     let label = UILabel()
@@ -12,11 +15,25 @@ class OnboardingViewController: UIViewController {
         style()
         layout()
     }
+    
+    init(heroImageName: String, titleText: String) {
+        self.heroImageName = heroImageName
+        self.titleText = titleText
+
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
 }
     
 extension OnboardingViewController {
     
     func style() {
+        
+        view.backgroundColor = .systemBackground
+        
         stackView.translatesAutoresizingMaskIntoConstraints = false
         stackView.axis = .vertical
         stackView.spacing = 20
@@ -24,7 +41,7 @@ extension OnboardingViewController {
         // image
         imageView.translatesAutoresizingMaskIntoConstraints = false
         imageView.contentMode = .scaleAspectFit
-        imageView.image = UIImage(named: "delorean")
+        imageView.image = UIImage(named: heroImageName)
         
         // label
         label.translatesAutoresizingMaskIntoConstraints = false
@@ -32,7 +49,7 @@ extension OnboardingViewController {
         label.font = UIFont.preferredFont(forTextStyle: .title1)
         label.adjustsFontForContentSizeCategory = true // хз что за свойство
         label.numberOfLines = 0
-        label.text = "Bankey is faster, easier to use, and has a brand new look and feel that will make you feel like you are back in 1989."
+        label.text = titleText
         
     }
     
